bcf5c71708e89719852e745e2cf2c7aaac4ba312
Alberto Lopez <albertolopez19@gmail.com>
2016-08-30 22:10:46 -0400

    Adding to existing lists now possible, modified URLS, view and template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -38,26 +38,67 @@ class NewListTest(TestCase):
             '/lists/new',
             data={'item_text': 'A new list item'}
         )
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
+
+
+class NewItemTest(TestCase):
+    """Test class for adding new items to an existing list."""
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertNotEqual(new_item.list, other_list)
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
 
 
 class ListViewTest(TestCase):
     """Test class for list views."""
 
-    def test_displays_all_items(self):
-        _list = List.objects.create()
-        Item.objects.create(text='itemey 1', list=_list)
-        Item.objects.create(text='itemey 2', list=_list)
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
-        print(response.content)
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list item 2')
 
     def test_uses_list_templates(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        _list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (_list.id,))
         self.assertTemplateUsed(response, 'list.html')
+
+    def test_passes_correct_list_to_template(self):
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+        self.assertEqual(response.context['list'], correct_list)
 
 
 class ListAndItemModelTest(TestCase):
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -8,14 +8,21 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request):
+def view_list(request, list_id):
     """Render the view list page."""
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+    _list = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': _list})
 
 
 def new_list(request):
     """Handler for creating a new list."""
     _list = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=_list)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect('/lists/%d/' % (_list.id,))
+
+
+def add_item(request, list_id):
+    """Handler for adding an item to an existing list."""
+    _list = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=_list)
+    return redirect('/lists/%d/' % (_list.id,))
