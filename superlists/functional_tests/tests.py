c64c02bba595ef8d155a0d2a0251aa524c1e9395
Alberto Lopez <albertolopez19@gmail.com>
2016-11-24 01:41:44 -0500

    Add static root folder in settings.py, disable admin for now

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,4 +1,5 @@
 """Functional tests for TO DO app."""
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
@@ -6,6 +7,22 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 class NewVisitorTest(StaticLiveServerTestCase):
     """Test a new user story."""
+
+    @classmethod
+    def setupClass(cls):
+        """Setup class from system argumets."""
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+            super().setupClass()
+            cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down steps for the test class."""
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -21,7 +38,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
@@ -44,7 +61,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a new online to do app. She checks out its
         # homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.server_url)
         # She notices the page title and header mention to do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -82,7 +99,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
 
         # Francis visits the home page. There is no sign of Ediths list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
